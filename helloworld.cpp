0cfc02cc8c480da29d0bc23b4fc75654259a7dda
Alex0ir <139131088+Oleks0ir@users.noreply.github.com>
2025-07-31 12:43:48 +0200

    first commit

---
+++ helloworld.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << endl << "Hello user! Welcome to HWrd-Tracker!" << endl << "This is a simple programm for tracking requests and their influence"
+     << endl  << "please refer to the documentation for documentation and basic concepts and abstractions we use at our Github!" << endl << endl;
+}
